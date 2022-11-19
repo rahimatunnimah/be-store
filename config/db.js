4d2bc6d2fcca7b998391edeec53801d420e80700
rahimatunnimah <rahimatunnimah@gmail.com>
2022-11-19 16:14:15 +0700

    revisi db to deploy

--- config/db.js
+++ config/db.js
@@ -1,16 +1,33 @@
-const Postgre = require("pg").Pool;
+const { Pool, Client } = require("pg");
+require("dotenv").config();
 
-const connection = new Postgre({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+console.log("process.env.ENV_MODE", process.env.ENV_MODE);
+let connection;
+if (process.env.ENV_MODE === "prod") {
+  connection = new Client({
+    connectionString: process.env.DB_URI,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  connection = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+const connect = async () => {
+  try {
+    const response = await connection.connect();
+    if (response) console.log("Connect to database");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+connect();
 module.exports = connection;
